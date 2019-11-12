ba8d4251bac745eef1cbc93ba0178ea0ef8506f9
jetly-design <jetly@amazon.com>
2019-11-12 21:58:54 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_7rmJF5xvT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2gdlg0bce6jciq6p97p36jrq4b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
